7615c1ff9465ac245d88d0e07ee92f5a5a8b0663
Camilly Souza <csouzasza@gmail.com>
2025-03-27 14:16:24 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+            ViewData["Professor"] = "Camilly Souza";
 
             return View();
         }
